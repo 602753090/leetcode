72f8b3070fb09d6d89ff9ff5b069fdf0d06fa212
caspian <vexento17@gmail.com>
2024-03-08 10:08:00 +0800

    Initial Commit

---
+++ leetcode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  leetcode
+//
+//  Created by 李雪峰 on 2024/3/8.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
