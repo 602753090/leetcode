6a5aaee5f1f5b245f2b855369f7b8c35417f2ae9
caspian <602753090@qq.com>
2024-03-19 19:04:59 +0800

    35.searchInsert and classification

---
+++ Simple/35.searchInsert/main.cpp
@@ -0,0 +1,47 @@
+//
+//  main.cpp
+//  35.searchInsert
+//
+//  Created by 李雪峰 on 2024/3/19.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int tail = (int)nums.size(), head = 0;
+        int mid = (head + tail) / 2;
+        while(head != mid) {
+            if(target > nums[mid]) {
+                head = mid;
+                mid = (head + tail) / 2;
+            }
+            if(target < nums[mid]) {
+                tail = mid;
+                mid = (head + tail) / 2;
+            }
+            if(target == nums[mid]) {
+                return mid;
+            }
+        }
+        if(target < nums[mid]) {
+            return mid;
+        }
+        if(target > nums[mid]){
+            return mid + 1;
+        }
+        return mid;
+    }
+};
+
+int main() {
+    vector<int> nums = {1};
+    int i = 0;
+    Solution s;
+    cin >> i;
+    cout << s.searchInsert(nums, i);
+    return 0;
+}
